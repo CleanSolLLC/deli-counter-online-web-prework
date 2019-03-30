52800391afb5684ac4a0a70a6f0c3ac86a54c028
Mark May <mamark90@gmail.com>
2019-03-30 00:57:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ new_katz_deli_array = []
   else
     katz_deli.each_with_index {|cust_name, index| index += 1
 
-    new_katz_deli_array.push (index, cust_name)
+    new_katz_deli_array.push(index,cust_name)
   }
       puts "The line is currently: #{new_katz_deli_array}"
   end
