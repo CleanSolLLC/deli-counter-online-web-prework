e58caa59a11b0503183a3eb066d213eac518bec0
Mark May <mamark90@gmail.com>
2019-03-30 01:26:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,8 +8,6 @@ new_string = []
     puts "The line is currently empty."
   else
     katz_deli.each_with_index {|cust_name, index| index += 1
-      line_num = index + "."
-      name = cust_name
     new_katz_deli_array.push(line_num,name)
     #new_katz_deli_array.join(".")
   }
